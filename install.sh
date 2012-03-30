b2b363004d6986c820bd024f8e5962aa15248604
fly2xj <fly2xj@msn.com>
2012-03-30 05:00:38 -0700

    aa

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
-echo "this is the first test of install script"
+echo "\033[0;33mDownload Font 4 U...\033[0m"
+/usr/bin/env git clone https://github.com/fly2xj/ubuntuChinesFont.git ~/.ubuntuCF
+
+if ! [ -d ~/.ubuntuCF ]; then 
+    echo "download failed. exit"
+    exit
+fi
+
+cd ~/.ubuntuCF
+

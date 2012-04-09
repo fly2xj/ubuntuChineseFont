8f44e8651509f518110e7d370db4d16044e2786d
fly2xj <fly2xj@msn.com>
2012-04-09 01:51:13 -0700

    sth

--- setTerminal.sh
+++ setTerminal.sh
@@ -9,14 +9,14 @@ BASHRC
 
 sudo apt-get install git-core
 /usr/bin/env git clone https://github.com/seebi/dircolors-solarized.git
-/usr/bin/env git clone https://github.com/coolwanglu/gnome-terminal-colors-solarizedusr/bin/env git clone https://github.com/seebi/dircolors-solarized.git
+/usr/bin/env git clone https://github.com/coolwanglu/gnome-terminal-colors-solarized.git
 
 cp dircolors-solarized/dircolors.256dark ~/.dircolors
 cd ~
 eval `dircolors .dircolors`
 
 cd gnome-terminal-colors-solarized 
-. set_dark.sh
+. ./set_dark.sh
 
 sudo apt-get install curl
 curl https://j.mp/spf13-vim3 -L -o - | sh
